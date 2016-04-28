bf6113ee24b36e7c078e9b418650a31b21c44f6a
RL <LBSMusicProd@Gmail.com>
2016-04-28 11:39:12 +0200

    Search done

--- web/app/view1/view1.js
+++ web/app/view1/view1.js
@@ -1,5 +1,27 @@
 'use strict';
-
+function populate(s1,s2){
+	var s1 = document.getElementById(s1);
+	var s2 = document.getElementById(s2);
+	s2.innerHTML = "";
+	if(s1.value == "CPH"){
+		var optionArray = ["|","SXF|Berlin","BCN|Barcelona","STN|London","CDG|Paris"];
+	} else if(s1.value == "SXF"){
+		var optionArray = ["|","CPH|Copenhagen","BCN|Barcelona","STN|London","CDG|Paris"];
+	} else if(s1.value == "BCN"){
+		var optionArray = ["|","CPH|Copenhagen","SXF|Berlin","STN|London","CDG|Paris"];
+        } else if(s1.value == "STN"){
+		var optionArray = ["|","CPH|Copenhagen","SXF|Berlin","BCN|Barcelona","CDG|Paris"];
+        }else if(s1.value == "CDG"){
+		var optionArray = ["|","CPH|Copenhagen","SXF|Berlin","BCN|Barcelona","STN|London"];
+        }
+	for(var option in optionArray){
+		var pair = optionArray[option].split("|");
+		var newOption = document.createElement("option");
+		newOption.value = pair[0];
+		newOption.innerHTML = pair[1];
+		s2.options.add(newOption);
+	}
+}
 angular.module('myApp.view1', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
@@ -14,8 +36,8 @@ angular.module('myApp.view1', ['ngRoute'])
     $scope.getFlights = function() {
         return $http({
             method: 'GET',
-            url: 'http://angularairline-plaul.rhcloud.com/api/flightinfo/CPH/2016-01-01T00:00:00.000Z/2',
-//            url: 'api/meta/' + $scope.from + '/' + $scope.dest + '/' + $scope.date + '/' + $scope.number,
+//            url: 'http://angularairline-plaul.rhcloud.com/api/flightinfo/CPH/2016-01-01T00:00:00.000Z/2',
+            url: 'api/meta/' + $scope.from + '/' + $scope.dest + '/' + $scope.date + '/' + $scope.number,
             skipAuthorization: true,
             contentType: "application/json"
         }).success(function(data, status, headers, config) {
