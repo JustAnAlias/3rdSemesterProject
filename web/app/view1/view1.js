ad2d999b2481c1e36ca266a109778c1489e2ce69
RL <LBSMusicProd@Gmail.com>
2016-04-28 09:06:05 +0200

    Search

--- web/app/view1/view1.js
+++ web/app/view1/view1.js
@@ -3,14 +3,29 @@
 angular.module('myApp.view1', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'app/view1/view1.html',
-    controller: 'View1Ctrl',
-    controllerAs : 'ctrl'
-  });
+    $routeProvider.when('/view1', {
+        templateUrl: 'app/view1/view1.html',
+        controller: 'View1Ctrl'
+    });
 }])
 
-.controller('View1Ctrl', ["InfoFactory","InfoService",function(InfoFactory,InfoService) {
-  this.msgFromFactory = InfoFactory.getInfo();
-  this.msgFromService = InfoService.getInfo();
-}]);
+.controller('View1Ctrl', ["$scope", "$http", function($scope, $http) {
+    $scope.output = {};
+    $scope.getFlights = function() {
+        return $http({
+            method: 'GET',
+            url: 'http://angularairline-plaul.rhcloud.com/api/flightinfo/CPH/2016-01-01T00:00:00.000Z/2',
+//            url: 'api/meta/' + $scope.from + '/' + $scope.dest + '/' + $scope.date + '/' + $scope.number,
+            skipAuthorization: true,
+            contentType: "application/json"
+        }).success(function(data, status, headers, config) {
+            $scope.output = data;
+        }).
+        error(function(data, status, headers, config) {
+        })
+    };
+
+
+
+
+}])
