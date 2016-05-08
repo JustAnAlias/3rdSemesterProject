4ee95607d56e5abc34dbea8190cfe744447bbe87
mrulle <michael.1978@gmail.com>
2016-05-08 23:49:04 +0200

    view3.js  post method redigeret

--- web/app/view3/view3.js
+++ web/app/view3/view3.js
@@ -2,44 +2,54 @@
 
 angular.module('myApp.view3', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view3', {
-    templateUrl: 'app/view3/view3.html',
-    controller: 'View3Ctrl'
-  });
-}])
+        .config(['$routeProvider', function ($routeProvider) {
+                $routeProvider.when('/view3', {
+                    templateUrl: 'app/view3/view3.html',
+                    controller: 'View3Ctrl'
+                });
+            }])
 
-.controller('View3Ctrl', ['$scope', '$http', 'InfoFactory', function ($scope, $http, InfoFactory) {
-        $scope.book = InfoFactory.getBooking();
+        .controller('View3Ctrl', ['$scope', '$http', 'InfoFactory', function ($scope, $http, InfoFactory) {
+                $scope.book = InfoFactory.getBooking();
 
-        console.log($scope.book);
+                console.log($scope.book);
 
-        
 
-        $scope.booking = function () {
 
-            console.log(($scope.book));
-            $scope.book.ReserveeName = $scope.username;
-            $scope.book.to_ = $scope.book.to;
-            
-            
+                $scope.booking = function () {
 
-            InfoFactory.setResponse(JSON.stringify($scope.book));
-//             console.log(InfoFactory.getResponse());
-//             $scope.data = response;
-//             console.log($scope.data);
-            
-//            $http.post("api/flightreservation", JSON.stringify($scope.book))
-//                    .success(function (response) {
-//                        InfoFactory.setResponse(response);
-//                        console.log(InfoFactory.getResponse());
-//                        $scope.data = response;
-//                        console.log($scope.data);
-//                    })
-//                    .error(function (response) {
-//                        $scope.data = "error";
-//                    });
-        };
-        
-    }]);
+                    console.log(($scope.book));
+                    $scope.book.ReserveeName = $scope.username;
+                    $scope.book.to_ = $scope.book.to;
+
+
+
+
+                    $http({
+                        method: 'POST',
+                        url: '/api/reservation/' + $scope.book.flightID,
+                        data: $scope.book
+                    }).then(function successCallback(response) {
+                        InfoFactory.setResponse(response);
+                    }, function errorCallback(response) {
+                        $scope.data = response;
+                    });
+                };
+//
+//
+//
+//                    $http.post("api/reservation" + $scope.book.flightID)
+//
+//                            .success(function (response) {
+//                                InfoFactory.setResponse(response);
+//                                console.log(InfoFactory.getResponse());
+//                                $scope.data = response;
+//                                console.log($scope.data);
+//                            })
+//                            .error(function (response) {
+//                                $scope.data = "error";
+//                            });
+
+
+            }]);
  
